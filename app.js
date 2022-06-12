b471a8ae4624b81b342ba726d7eb26953d3f31fd
Alper Çıtak <citak.alper@gmail.com>
2022-06-12 13:57:12 +0300

    Feature/codestyle (#1)
    * added prettier
    
    * added eslint
    
    * added husky & lint-staged
    
    * formatting
    
    * updated lint-staged
    
    * updated package.json

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 
 const config = {
-    "dirname": __dirname,
-    "name": process.env.NAME,
-    "title": process.env.NAME_READABLE,
-    "http": {
-        port: process.env.PORT
-    }
+  dirname: __dirname,
+  name: process.env.NAME,
+  title: process.env.NAME_READABLE,
+  http: {
+    port: process.env.PORT,
+  },
 };
 
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", "./views");
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 app.use('/assets', express.static(config.dirname + '/public/assets/'));
 app.use(express.static(config.dirname + '/public/'));
 
 app.use((req, res, next) => {
-    res.locals.title = config.title;
-    return next();
+  res.locals.title = config.title;
+  return next();
 });
-app.get("/", (req, res) => {
-    return res.render("home/index");
+app.get('/', (req, res) => {
+  return res.render('home/index');
 });
 
 app.listen(config.http.port);
